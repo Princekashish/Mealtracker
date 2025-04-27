de4be9dedfc13c478417dc1c6671666e995d4d35
Prince <princekashishbbu@gmail.com>
2025-04-27 23:20:29 +0530

    few changes

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,10 +22,12 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode;
 }>) {
+  //suppressHydrationWarning
   return (
     <html lang="en">
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        suppressHydrationWarning
       >
         {children}
       </body>
---
+++ src/components/section-0.tsx
@@ -0,0 +1,8 @@
+"use client";
+export const Section=()=>{
+return(
+    <div className="bg-amber-800 h-screen flex justify-center items-center text-4xl text-white font-semibold">
+        <h1 >Prince kashish</h1>
+    </div>
+)
+}
