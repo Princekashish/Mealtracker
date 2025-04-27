8026fd20f3da56eb8be6ec7eea0b11a15946da27
Prince <princekashishbbu@gmail.com>
2025-04-27 23:41:56 +0530

    refactor

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import { Section1 } from "@/components/section-1";
+import React from "react";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <Section1 />
+    </div>
+  );
+}
---
+++ src/components/section-1.tsx
@@ -0,0 +1,8 @@
+"use client";
+export const Section1 = () => {
+  return (
+    <div>
+      <h1>Dashboard</h1>
+    </div>
+  );
+};
